dc6b8aefac394c1452b83d37029c6fdfe2b227fd
VICTOR BARBULESCU <victor.barbulescu23@CLS-APP-08.local>
2022-08-17 08:09:25 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var byteDemo = 1
+    var shortDemo = 5
+    var intDemo = 12
+    var longDemo = 30L
+    var floatDemo = 3.14
+    var doubleDemo = 2.1954321
+    var booleanDemo = false
+    var charDemo = 'q'
+    
+    println("Byte demo: $byteDemo")
+    println("Short demo: $shortDemo")
+    println("Int demo: $intDemo")
+    println("Long demo: $longDemo")
+    println("Float demo: $floatDemo")
+    println("Double demo: $doubleDemo")
+    println("Boolean demo: $booleanDemo")
+    println("Char demo: $charDemo")
+
+}
